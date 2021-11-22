bdf1ae64205877476dfd732a0775b307dd058c60
Jungtaek Kim <jtkim@postech.ac.kr>
2021-11-22 11:54:10 +0900

    Remove tranvis for Python 2.7 and 3.5

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     license='MIT',
     description='Benchmarks for Bayesian optimization',
     packages=list_packages,
-    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, != 3.3.*, !=3.4.*, <4',
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, != 3.3.*, !=3.4.*, !=3.5.*, <4',
     install_requires=required,
     classifiers=[
         'License :: OSI Approved :: MIT License',
