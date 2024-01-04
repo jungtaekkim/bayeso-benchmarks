5b26b1d17c88eaba0f84eb33f1c245363a26fecd
Jungtaek Kim <jungtaek.kim@pitt.edu>
2024-01-04 14:20:36 -0500

    Happy new year

--- bayeso_benchmarks/__init__.py
+++ bayeso_benchmarks/__init__.py
@@ -1,5 +1,5 @@
 #
-# author: Jungtaek Kim (jtkim@postech.ac.kr)
+# author: Jungtaek Kim (jungtaek.kim.mail@gmail.com)
 # last updated: January 27, 2023
 #
 
--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
 from setuptools import setup
+from pathlib import Path
+
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 path_requirements = 'requirements.txt'
 list_packages = ['bayeso_benchmarks']
@@ -10,10 +15,12 @@ setup(
     name='bayeso-benchmarks',
     version='0.2.0',
     author='Jungtaek Kim',
-    author_email='jtkim@postech.ac.kr',
+    author_email='jungtaek.kim.mail@gmail.com',
     url='https://bayeso.org',
     license='MIT',
-    description='Benchmarks for Bayesian optimization',
+    description='Benchmark Functions for Bayesian optimization',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     packages=list_packages,
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, != 3.3.*, !=3.4.*, !=3.5.*, <4',
     install_requires=required,
